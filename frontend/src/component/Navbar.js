43f8212c269268f17556b65e5437387f41a5baac
Jonathan Rudich <rudich.jonathan@gmail.com>
2020-11-08 15:00:00 +0800

    created components and view folders, created navbar component

---
+++ frontend/src/component/Navbar.js
@@ -0,0 +1,26 @@
+
+
+
+const Navbar = () => {
+    return(
+        <Menu pointing secondary vertical>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='messages'
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='friends'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+    );
+}
+
+export default Navbar;
