4e7552ec45d51c6be5c00dacfd71f31d3af40880
Jeremy DePoyster <jdepoyster@gmail.com>
2020-11-23 19:32:05 -0500

    char and map update

--- frontend/src/component/Chart.js
+++ frontend/src/component/Chart.js
@@ -13,7 +13,7 @@ import axios from "axios";
 const processData = (data) => {
   return data.map((e) => ({
     x: e.year,
-    y: e.lv,
+    y: e.dollarval,
   }));
 };
 
@@ -21,22 +21,52 @@ export default class Chart extends Component {
   state = {
     isLoaded: false,
     nhoods: [],
-    year_built_before: 2020,
-    year_built_after: 1900,
+    year_built_bw_first: 1900,
+    year_built_bw_sec: 2020,
     ncode: 0,
-    zcategory: '',
-    selection: ''
+    zcategory: "",
+    selection: "lv",
+    math: "avg",
   };
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({isLoaded: false})
+    axios
+      .get("http://localhost:5000/api/charts", {
+        params: {
+          selection: this.state.selection,
+          math: this.state.math,
+          // year_built_bw_first: this.state.year_built_bw_first,
+          // year_built_bw_sec: this.state.year_built_bw_sec,
+        },
+      })
+      .then((res) => {
+        const nhoods = res.data;
+        this.setState({ nhoods });
+        this.setState({ isLoaded: true });
+      });
+  }
 
   componentDidMount() {
     axios
-      .get('http://localhost:5000/api/charts', {
+      .get("http://localhost:5000/api/charts", {
         params: {
-          selection: 'lv',
-          math: 'avg'
-        }
-      }
-      )
+          selection: this.state.selection,
+          math: this.state.math,
+          // year_built_bw_first: this.state.year_built_bw_first,
+          // year_built_bw_sec: this.state.year_built_bw_sec,
+        },
+      })
       .then((res) => {
         const nhoods = res.data;
         this.setState({ nhoods });
@@ -47,32 +77,61 @@ export default class Chart extends Component {
   render() {
     return (
       <div>
-        <XYPlot
-          width={800}
-          height={600}
-          xDomain={[2006, 2020]}
-          margin={{ left: 100, right: 100 }}
-        >
-          <HorizontalGridLines style={{ stroke: "#B7E9ED" }} />
-          <VerticalGridLines style={{ stroke: "#B7E9ED" }} />
-          <XAxis
-            style={{
-              line: { stroke: "#ADDDE1" },
-              ticks: { stroke: "#ADDDE1" },
-              text: { stroke: "none", fill: "#6b6b76", fontWeight: 600 },
-            }}
-            tickFormat={(v) => `${v}`}
-          />
-          <YAxis title="$ CAD" />
-          <LineSeries
-            className="first-series"
-            data={this.state.isLoaded ? processData(this.state.nhoods) : null}
-            style={{
-              strokeLinejoin: "round",
-              strokeWidth: 4,
-            }}
-          />
-        </XYPlot>
+        <div>
+          <XYPlot
+            width={800}
+            height={600}
+            xDomain={[2006, 2020]}
+            margin={{ left: 100, right: 100 }}
+          >
+            <HorizontalGridLines style={{ stroke: "#B7E9ED" }} />
+            <VerticalGridLines style={{ stroke: "#B7E9ED" }} />
+            <XAxis
+              style={{
+                line: { stroke: "#ADDDE1" },
+                ticks: { stroke: "#ADDDE1" },
+                text: { stroke: "none", fill: "#6b6b76", fontWeight: 600 },
+              }}
+              tickFormat={(v) => `${v}`}
+            />
+            <YAxis title="$ CAD" />
+            <LineSeries
+              className="first-series"
+              data={
+                this.state.isLoaded
+                  ? processData(this.state.nhoods)
+                  : [{ x: 0, y: 0 }]
+              }
+              style={{
+                strokeLinejoin: "round",
+                strokeWidth: 4,
+              }}
+            />
+          </XYPlot>
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <select
+              name="selection"
+              value={this.state.selection}
+              onChange={this.handleChange.bind(this)}
+            >
+              <option value="lv">Land Value</option>
+              <option value="tl">Tax Levy</option>
+              <option value="iv">Improvement Value</option>
+            </select>
+            <select
+              name="math"
+              value={this.state.math}
+              onChange={this.handleChange.bind(this)}
+            >
+              <option value="avg">Average</option>
+              <option value="median">Median</option>
+              <option value="max">Max</option>
+            </select>
+            <input type="submit" value="Search" />
+          </form>
+        </div>
       </div>
     );
   }
--- frontend/src/component/Map.js
+++ frontend/src/component/Map.js
@@ -5,21 +5,56 @@ import {
   Popup,
   MapConsumer,
   MapContainer,
+  GeoJSON,
 } from "react-leaflet";
+import axios from "axios";
 
 export default class Map extends Component {
+  state = {
+    isLoaded: false,
+    outlines: [],
+    outmaps: []
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/fillmap").then((res) => {
+      const outlines = res.data;
+      this.setState({ outlines });
+      console.log(outlines);
+      this.setState({ isLoaded: true });
+
+    });
+  }
+
   render() {
     return (
-      <MapContainer
-        center={[49.246576, -123.090169]}
-        zoom={12}
-        style={{ width: "100%", height: "500px" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      <div>
+        <MapContainer
+          center={[49.246576, -123.090169]}
+          zoom={12}
+          style={{ width: "100%", height: "500px" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          
+          // Check if data fetched
+          {this.state.isLoaded ? 
+          <GeoJSON
+            data={
+              this.state.outlines.map(x => x.geo_poly_outline)
+            }
+            style={() => ({
+              weight: 2,
+              color: "#666",
+              dashArray: "",
+              fillOpacity: 0.7,
+            })}
+          /> : <h1> Loading </h1>}
+          
+        </MapContainer>
+      </div>
     );
   }
 }
